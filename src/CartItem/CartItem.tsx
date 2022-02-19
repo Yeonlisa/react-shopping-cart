7bf8a1bfd9aba438bb4a0dadb80b55928ff17d57
yeonlisa <tkzw4869@gmail.com>
2022-02-19 21:41:06 +0900

    Item,Cart,CartItem

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,9 @@
+import Button from '@material-ui/core/Button';
+// Types
+import { CartItemType } from '../App';
+// Stylesw
+import { Wrapper } from './CartItem.styles';
+
+const CartItem: React.FC = () => <div>Cart Item</div>
+
+export default CartItem;
